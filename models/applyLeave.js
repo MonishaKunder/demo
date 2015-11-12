16a39c350540ecf5f3556ef0c347d0cffa6690a0
vikram thomas <vikramthomas93@gmail.com>
2015-11-12 01:45:47 -0500

    modified apply leave to add sessions

--- models/applyLeave.js
+++ models/applyLeave.js
@@ -5,27 +5,24 @@ require('../schema');
 module.exports=function(req,res){
 	
 		var obj=req.body;
-		var id=obj.uniqueid;
+		var id=req.headers.uniqueid;
 		var days;
-		var halfdays;
+		var halfdays=0;
 		var dFrom=obj.from.split('-');
 		var dTo=obj.to.split('-');
-		/*
-		if(obj.from.halfday==true) {
+		
+
+		if(obj.fromSession=='Session 1'||obj.fromSession=='Session 2') {
 			halfdays += 0.5;
 		}
-		if(obj.to.halfday==true) {
+		if(obj.toSession=='Session 1'||obj.toSession=='Session 2') {
 			halfdays += 0.5;
-		}
-		*/
-		if(obj.halfday==true) {
-			days = 0.5
-		}
-		else {
-			days=(new Date(dTo[0],dTo[1]-1,dTo[2])- new Date(dFrom[0],dFrom[1]-1,dFrom[2]))/(3600*24*1000)+1;
 		}
 
-		//days -= halfdays;
+		days=(new Date(dTo[0],dTo[1]-1,dTo[2])- new Date(dFrom[0],dFrom[1]-1,dFrom[2]))/(3600*24*1000)+1;
+		
+		days -= halfdays;
+		
 		async.waterfall([
 			function(callback){
 				user.findById(id,{'organizationaldata.manager':1,personaldata:1,username:1},function(err,doc){
@@ -36,11 +33,11 @@ module.exports=function(req,res){
 				})
 			},
 			function(doc,callback){
-				console.log(doc.username);
+				console.log(doc);
 				var lObj={
 					appliedOn:new Date(Date.now()),
-					from:new Date(obj.from).toISOString(),
-					to:new Date(obj.to).toISOString(),
+					from: {day : new Date(obj.from).toISOString(),session:obj.fromSession},
+					to: {day : new Date(obj.to).toISOString(),session:obj.toSession},
 					type:obj.type,
 					halfDay:obj.halfday,
 					applierId:{displayName:doc.personaldata['first name']+" "+doc.personaldata['last name'],
